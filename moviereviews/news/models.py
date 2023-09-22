29996d0182195a6ae0f28e88e157686d1c8d4a9c
BhaskarR <Bhaskar.programaddict@gmail.com>
2023-09-22 07:36:44 +0530

    base template added

--- moviereviews/news/models.py
+++ moviereviews/news/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class News(models.Model):
+    headlines = models.CharField(max_length=200)
+    body = models.TextField()
+    date = models.DateField()
+
+    def __str__(self) -> str:
+        return self.headlines
+
