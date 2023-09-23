0329710fd10effd2dda59eae658f5526e0b87abe
BhaskarR <Bhaskar.programaddict@gmail.com>
2023-09-23 07:25:47 +0530

    login functionality implemented

--- moviereviews/accounts/views.py
+++ moviereviews/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 from django.shortcuts import redirect
 from django.db import IntegrityError
 from .forms import UserCreateForm
@@ -29,3 +30,16 @@ def signupaccount(request):
 def logoutaccount(request):
     logout(request)
     return redirect('home')
+
+
+def loginaccount(request):
+    if request.method == 'GET':
+        return render(request, 'loginaccount.html', {'form': AuthenticationForm})
+    else:
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'loginaccount.html', {'form': AuthenticationForm(), 'error': 'username and password donot match'})
+        else:
+            login(request, user)
+            return redirect('home')
